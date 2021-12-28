d91b2a0bb31ddefd13148123f839c94230993941
Chung <52872325+HiamKaito@users.noreply.github.com>
2021-12-28 12:14:22 +0700

    Base

---
+++ QLTTNN/BUS/ThiSinh.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DAL;
+
+namespace BUS
+{
+    public class ThiSinh
+    {
+        public string MaCMND { get; set; }
+        public string HoTen { get; set; }
+        public string GioiTinh { get; set; }
+        public string NgaySinh { get; set; }
+        public string NoiSinh { get; set; }
+        public string SDT { get; set; }
+
+        public ThiSinh(){}
+
+        public static int getCount()
+        {
+            return 0;
+        }
+
+        public List<ThiSinh> getAll()
+        {
+            return ThiSinhDAL.getAll();
+        }
+
+
+    }
+}
---
+++ QLTTNN/DAL/ThiSinhDAL.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BUS;
+
+namespace DAL
+{
+    public class ThiSinhDAL
+    {
+        public static int getCount()
+        {
+            /*
+            using (DatabaseDataContext db = new DatabaseDataContext())
+            {
+                int count = (from u in db.LOAIHINHDULICHes select u).Count();
+                return 0;
+            }
+            */
+            return 0;
+        }
+
+        public static List<ThiSinh> getAll()
+        {
+            List<ThiSinh> list = new List<ThiSinh>();
+            /*
+            using (DatabaseDataContext db = new DatabaseDataContext())
+            {
+                var result = from u in db.LOAIHINHDULICHes
+                             select new
+                             {
+                                 MaLoaiHinh = u.MaLoaiHinh,
+                                 TenLoaiHinh = u.TenLoaiHinh,
+                             };
+
+                foreach (var i in result)
+                {
+                    LoaiHinhDuLichModel tour = new LoaiHinhDuLichModel();
+
+                    tour.MaLoaiHinh = i.MaLoaiHinh;
+                    tour.TenLoaiHinh = i.TenLoaiHinh;
+                    list.Add(tour);
+                }
+            }
+            */
+            return list;
+        }
+    }
+}
